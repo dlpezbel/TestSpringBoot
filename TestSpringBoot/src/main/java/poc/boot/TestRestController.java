3d3d47d2cc8194e6dc94e5cc723df9b0472ea814
dlpezbel <dlpezbel@yahoo.es>
2016-12-05 17:08:32 +0100

    v1.0

--- TestSpringBoot/src/main/java/poc/boot/TestRestController.java
+++ TestSpringBoot/src/main/java/poc/boot/TestRestController.java
@@ -1,13 +1,22 @@
 package poc.boot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import poc.boot.dto.Order;
 
 @RestController
 public class TestRestController {
 
-    @RequestMapping("/test")
-    public String index() {
+    @RequestMapping(value = "/test/{orderId}", method = RequestMethod.GET, produces = "application/json")
+    public String getOrder(@PathVariable("orderId") Integer orderId) {
+    	System.out.println("get orderId: " + orderId);
+    	Order o = new Order();
+    	o.setId(orderId);
+    	o.setName("test");
         return "index";
     }
 
